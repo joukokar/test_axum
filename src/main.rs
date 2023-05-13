f8d996b91b50a483f8c9e6361f654a6ae9d56c6a
Pyry Kontio <pyry.kontio@drasa.eu>
2023-05-13 16:30:15 +0900

    Jollain tasolla toimii

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
+use std::sync::Arc;
+
+use sqlx::{postgres::PgPoolOptions, Postgres, Pool};
+
 use axum::{
     routing::get,
     Router,
+    Json, http::StatusCode, extract::State,
 };
+
+
+async fn hello_handler(
+    State(pool): State<Arc<Pool<Postgres>>>
+) -> (StatusCode, Json<()>) {
+    println!("helole");
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&*pool).await.unwrap();
+
+    println!("Row: {}", row.0);
+
+    (StatusCode::OK, Json(()))
+}
+
+
 
 #[tokio::main]
 async fn main() {
+    let pool = Arc::new(PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://postgres:password@127.0.0.1:5432/database").await.unwrap());
+
+    println!("pool done");
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(hello_handler)).with_state(pool);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
