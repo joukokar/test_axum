8dc94beab1b2d8fbab3fd971e6583009e0ab4f35
Jouko Karvonen <joukokar@gmail.com>
2023-05-13 16:59:08 +0900

    Add postgres connection and param getting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use axum::{extract::State, routing::get, Router};
+use axum::{
+    extract::{Path, State},
+    routing::get,
+    Router,
+};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use std::sync::{
     atomic::{AtomicU32, Ordering},
     Arc,
@@ -7,15 +12,31 @@ use std::sync::{
 #[derive(Debug)]
 struct AppState {
     visitor_count: AtomicU32,
+    pool: Pool<Postgres>,
 }
 
 #[tokio::main]
 async fn main() {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost:25432/postgres")
+        .await
+        .unwrap();
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    println!("{:?}", row.0);
+
     let shared_state = Arc::new(AppState {
         visitor_count: 0.into(),
+        pool: pool,
     });
     // build our application with a single route
-    let app = Router::new().route("/", get(handler).with_state(shared_state));
+    let app = Router::new().route("/:user_id", get(handler).with_state(shared_state));
+    // .route("/posts", get(get_posts));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -24,11 +45,24 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler(State(state): State<Arc<AppState>>) -> String {
+async fn handler(Path(user_id): Path<String>, State(state): State<Arc<AppState>>) -> String {
+    println!("user_id: {}", user_id);
     let new_state = state.as_ref();
     new_state.visitor_count.fetch_add(1, Ordering::SeqCst);
     println!("{:?}", new_state.visitor_count);
     let c = new_state.visitor_count.load(Ordering::SeqCst);
     let respo = format!("There have been {} visitors.", c);
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(152_i64)
+        .fetch_one(&new_state.pool)
+        .await
+        .unwrap();
+    println!("{:?}", row.0);
+
     respo
+}
+
+async fn get_posts(State(state): State<Arc<AppState>>) -> String {
+    "".to_string()
 }
